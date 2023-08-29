62547aa4afc79ca6680282821bddbaccd43eadd2
Aurelia-Perrier <aurelia.perrier17@gmail.com>
2023-08-29 15:59:10 +0200

    dynamisation de la liste avec un tableau de données fictives

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,14 @@ import './styles.scss';
 import CounterTasks from '../CounterTasks';
 import Form from '../Form';
 import TasksList from '../TasksList';
+import tasks from '../../data/tasks';
 
 function App() {
   return (
     <div className="app">
       <Form />
       <CounterTasks />
-      <TasksList />
+      <TasksList tasks={tasks} />
     </div>
 
   );
