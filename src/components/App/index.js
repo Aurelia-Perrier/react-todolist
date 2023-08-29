fb43fb1a89f47ab585adfb19d92e1b322f1b8bb4
Aurelia-Perrier <aurelia.perrier17@gmail.com>
2023-08-29 16:35:46 +0200

    ajout d'un state en utilisant useState + ajout d'une tache dans la liste

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,46 @@
 import './styles.scss';
+import { useState } from 'react';
 import CounterTasks from '../CounterTasks';
 import Form from '../Form';
 import TasksList from '../TasksList';
-import tasks from '../../data/tasks';
+import tasksData from '../../data/tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskLabel, setTaskLabel] = useState('');
+
+  const generateNewId = () => {
+    if (tasks.length === 0) {
+      return 1;
+    }
+    const idList = tasks.map((task) => task.id);
+    return Math.max(...idList) + 1;
+  };
+
+  const handleNewLabel = (event) => {
+    setTaskLabel(event.target.value);
+  };
+
+  const addTask = (event) => {
+    event.preventDefault();
+
+    const newTask = {
+      id: generateNewId(),
+      label: taskLabel,
+      done: false,
+    };
+
+    const tasksCopy = [...tasks];
+    tasksCopy.push(newTask);
+
+    setTasks(tasksCopy);
+    setTaskLabel('');
+  };
+
   const toDoTasks = tasks.filter((task) => task.done === false);
   return (
     <div className="app">
-      <Form />
+      <Form addTask={addTask} handleNewLabel={handleNewLabel} taskLabel={taskLabel} />
       <CounterTasks toDoTasks={toDoTasks.length} />
       <TasksList tasks={tasks} />
     </div>
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,15 +1,24 @@
+import PropTypes from 'prop-types';
 import './styles.scss';
 
-function Form() {
+function Form({ addTask, handleNewLabel, taskLabel }) {
   return (
-    <form>
+    <form onSubmit={addTask}>
       <input
         type="text"
         className="form-input"
         placeholder="Ajouter une tâche"
+        onChange={handleNewLabel}
+        value={taskLabel}
       />
     </form>
   );
 }
+
+Form.propTypes = {
+  addTask: PropTypes.func.isRequired,
+  handleNewLabel: PropTypes.func.isRequired,
+  taskLabel: PropTypes.string.isRequired,
+};
 
 export default Form;
