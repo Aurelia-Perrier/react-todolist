85914146df7c80aa81cd64f500a9422b0aa87af7
Aurelia-Perrier <aurelia.perrier17@gmail.com>
2023-08-29 16:04:04 +0200

    dynamisation du compteur de taches

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,10 +5,11 @@ import TasksList from '../TasksList';
 import tasks from '../../data/tasks';
 
 function App() {
+  const toDoTasks = tasks.filter((task) => task.done === false);
   return (
     <div className="app">
       <Form />
-      <CounterTasks />
+      <CounterTasks toDoTasks={toDoTasks.length} />
       <TasksList tasks={tasks} />
     </div>
 
--- src/components/CounterTasks/index.js
+++ src/components/CounterTasks/index.js
@@ -1,9 +1,14 @@
 import './styles.scss';
+import PropTypes from 'prop-types';
 
-function CounterTasks() {
+function CounterTasks({ toDoTasks }) {
   return (
-    <h1 className="counter">3 tâches à effectuer</h1>
+    <h1 className="counter">{toDoTasks} tâches à effectuer</h1>
   );
 }
+
+CounterTasks.propTypes = {
+  toDoTasks: PropTypes.number.isRequired,
+};
 
 export default CounterTasks;
